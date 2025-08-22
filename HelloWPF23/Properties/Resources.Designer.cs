88ae444649b32e57787fbdd186bcebc9067bf9d2
andrew <g6961116@gmail.com>
2025-08-22 22:28:23 +0800

    remove unused using

--- HelloWPF23/Properties/Resources.Designer.cs
+++ HelloWPF23/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Claculator.Properties {
+namespace Calculator.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Claculator.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("HelloWPF23.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Calculator.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- HelloWPF23/Properties/Settings.Designer.cs
+++ HelloWPF23/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Claculator.Properties {
+namespace Calculator.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
